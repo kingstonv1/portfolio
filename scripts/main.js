918049c61e171f637a8b09eac0df5752cc0fd08b
kingstonv1 <kingstongise@gmail.com>
2022-11-15 14:51:02 -0500

    update buttons

--- scripts/main.js
+++ scripts/main.js
@@ -14,5 +14,4 @@ if (isMobile()) {
 //document.getElementById('home').addEventListener("click", function () { document.getElementById('top').scrollIntoView({behavior: 'smooth'}); });
 document.getElementById('aboutme').addEventListener("click", function () { document.getElementById('top').scrollIntoView({ behavior: 'smooth' }); });
 document.getElementById('projects').addEventListener("click", function () { document.getElementById('projs').scrollIntoView({ behavior: 'smooth' }); });
-//Change this to "Contact" later
-document.getElementById('contact').addEventListener("click", function () { document.getElementById('top').scrollIntoView({ behavior: 'smooth' }); });
+document.getElementById('contact').addEventListener("click", function () { document.getElementById('contactinfo').scrollIntoView({ behavior: 'smooth' }); });
